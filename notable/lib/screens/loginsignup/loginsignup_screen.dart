15d651af88228dfceec72bd7000d6a9872feef1f
yianniskoutras <ioanniskoutras2002@gmail.com>
2024-01-14 20:32:21 +0200

    Added a loading aspect between the loginsignup screen and homepage. TODO: same as previous push -> an exw xrono that ta kanw egw to vrady

--- notable/lib/screens/loginsignup/loginsignup_screen.dart
+++ notable/lib/screens/loginsignup/loginsignup_screen.dart
@@ -9,115 +9,161 @@ import 'package:firebase_auth/firebase_auth.dart';
 import '../../app_state.dart';
 import 'package:provider/provider.dart';
 
-// ignore_for_file: must_be_immutable
-class LoginsignupScreen1 extends StatelessWidget {
+class LoginsignupScreen1 extends StatefulWidget {
   LoginsignupScreen1({Key? key}) : super(key: key);
 
+  @override
+  _LoginsignupScreen1State createState() => _LoginsignupScreen1State();
+}
+
+class _LoginsignupScreen1State extends State<LoginsignupScreen1> {
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
-
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  bool loading = false;
 
   @override
   Widget build(BuildContext context) {
-    mediaQueryData = MediaQuery.of(context);
     return SafeArea(
-        child: Scaffold(
-            resizeToAvoidBottomInset: false,
-            body: Form(
-                key: _formKey,
-                child: Container(
-                    width: double.maxFinite,
-                    padding: EdgeInsets.only(left: 25.h, top: 158.v, right: 25.h),
-                    child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-                      Align(
-                          alignment: Alignment.center,
-                          child: Text("Welcome to Notable", style: theme.textTheme.displaySmall)),
-                      Container(
-                          width: 288.h,
-                          margin: EdgeInsets.only(left: 20.h, right: 31.h),
-                          child: Text("Login/Create an account to enjoy all the services without any ads for free!",
-                              maxLines: 2,
-                              overflow: TextOverflow.ellipsis,
-                              textAlign: TextAlign.center,
-                              style: theme.textTheme.bodyMedium!.copyWith(height: 1.30))),
-                      SizedBox(height: 41.v),
-                      Padding(
-                          padding: EdgeInsets.only(right: 7.h),
-                          child: CustomTextFormField(
-                              controller: emailController,
-                              hintText: "your.org@mail.com",
-                              textInputAction: TextInputAction.done,
-                              textInputType: TextInputType.emailAddress)),
-                      SizedBox(height: 8.v),
-                      Padding(
-                          padding: EdgeInsets.only(right: 7.h),
-                          child: CustomTextFormField(
-                            controller: passwordController,
-                            hintText: "YourStrongPassword",
-                            textInputAction: TextInputAction.done,
-                            textInputType: TextInputType.visiblePassword,
-                            obscureText: true,
-                          )),
-                      SizedBox(height: 8.v),
-                      CustomElevatedButton(
-                          text: "Login",
-                          margin: EdgeInsets.only(left: 29.h, right: 39.h),
-                          onPressed: () {
-                            onTapLogin(context);
-                          },
-                          alignment: Alignment.center),
-                      Center(
-                          child: TextButton(
-                              onPressed: () {
-                                onTapCreateAnAccount(context);
-                              },
-                              child: Text("Create an account",
-                                  style: TextStyle(
-                                    color: Theme.of(context).primaryColor, // Adjust the color as needed
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 16.fSize, // Adjust the font size as needed
-                                  )))),
-                      Center(
-                          child: TextButton(
-                              onPressed: () =>onTapForgotPasswordAlert(
-                                  context: context,
-                                  title: 'Forgot Password',
-                                  icon: const Icon(Icons.mail_outline),
-                                  labelText: 'Enter your email',
-                                  buttonText: 'Send',),
-                              child: Text("Forgot your password?",
-                                  style: TextStyle(
-                                    color: Colors.blue, // Adjust the color as needed
-                                    fontSize: 16.fSize, // Adjust the font size as needed
-                                  )))),
-                    ])))));
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: Stack(
+          children: [
+            Form(
+              key: _formKey,
+              child: Container(
+                width: double.maxFinite,
+                padding: EdgeInsets.only(left: 25.h, top: 158.v, right: 25.h),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Align(
+                      alignment: Alignment.center,
+                      child: Text("Welcome to Notable", style: theme.textTheme.displaySmall),
+                    ),
+                    Container(
+                      width: 288.h,
+                      margin: EdgeInsets.only(left: 20.h, right: 31.h),
+                      child: Text(
+                        "Login/Create an account to enjoy all the services without any ads for free!",
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                        textAlign: TextAlign.center,
+                        style: theme.textTheme.bodyMedium!.copyWith(height: 1.30),
+                      ),
+                    ),
+                    SizedBox(height: 41.v),
+                    Padding(
+                      padding: EdgeInsets.only(right: 7.h),
+                      child: CustomTextFormField(
+                        controller: emailController,
+                        hintText: "your.org@mail.com",
+                        textInputAction: TextInputAction.done,
+                        textInputType: TextInputType.emailAddress,
+                      ),
+                    ),
+                    SizedBox(height: 8.v),
+                    Padding(
+                      padding: EdgeInsets.only(right: 7.h),
+                      child: CustomTextFormField(
+                        controller: passwordController,
+                        hintText: "YourStrongPassword",
+                        textInputAction: TextInputAction.done,
+                        textInputType: TextInputType.visiblePassword,
+                        obscureText: true,
+                      ),
+                    ),
+                    SizedBox(height: 8.v),
+                    CustomElevatedButton(
+                      text: "Login",
+                      margin: EdgeInsets.only(left: 29.h, right: 39.h),
+                      onPressed: () {
+                        onTapLogin(context);
+                      },
+                      alignment: Alignment.center,
+                    ),
+                    SizedBox(height: 5),
+                    Center(
+                      child: TextButton(
+                        onPressed: () {
+                          onTapCreateAnAccount(context);
+                        },
+                        child: Text(
+                          "Create an account",
+                          style: TextStyle(
+                            color: Theme.of(context).primaryColor,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16.fSize,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 5),
+                    Center(
+                      child: TextButton(
+                        onPressed: () => onTapForgotPasswordAlert(
+                          context: context,
+                          title: 'Forgot Password',
+                          icon: const Icon(Icons.mail_outline),
+                          labelText: 'Enter your email',
+                          buttonText: 'Send email',
+                        ),
+                        child: Text(
+                          "Forgot your password?",
+                          style: TextStyle(
+                            color: Colors.blue,
+                            fontSize: 16.fSize,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            if (loading)
+              Container(
+                color: Colors.black.withOpacity(0.5),
+                child: Center(
+                  child: CircularProgressIndicator(),
+                ),
+              ),
+          ],
+        ),
+      ),
+    );
   }
 
-  /// Navigates to the homepageScreen when the action is triggered.
   onTapLogin(BuildContext context) async {
     if (_formKey.currentState?.validate() ?? false) {
       try {
+        setState(() {
+          loading = true;
+        });
+
         User? currUser = await AuthService().signIn(
           emailController.text,
           passwordController.text,
         );
-        // Navigate to the homepageScreen upon successful login
+
         if (currUser != null) {
           Navigator.pushNamed(context, AppRoutes.homepageScreen);
         } else {
           print('user was null');
         }
+
         Provider.of<AppState>(context, listen: false).setUser(currUser);
       } catch (e) {
-        // Handle login errors (display error message or take appropriate action)
-        // todo make the Forgot Password visible
         print('Login Error: $e');
+        // Handle login errors (display error message or take appropriate action)
+      } finally {
+        setState(() {
+          loading = false;
+        });
       }
     }
   }
 
-  /// Navigates to the createAccountScreen when the action is triggered.
   onTapCreateAnAccount(BuildContext context) {
     Navigator.pushNamed(context, AppRoutes.signupScreen);
   }
